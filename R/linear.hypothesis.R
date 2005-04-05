56d9ffe1972f6d3aecf256e4bd39b8b4a3189910
John Fox <jfox@mcmaster.ca>
2005-04-05 00:48:54 +0000

    version 1.0-16

--- R/linear.hypothesis.R
+++ R/linear.hypothesis.R
@@ -1,6 +1,8 @@
 # Linear hypothesis tests for lm and glm (J. Fox)
-
 # last modified 9 Nov 02
+# 2005-03-17: modified by Achim Zeleis to include `test' and `vcov' arguments,
+#             and prettified output
+
 
 lht<-function(...) linear.hypothesis(...)
 
@@ -10,60 +12,117 @@ linear.hypothesis<-function (model, ...) {
     
 linear.hypothesis.lm<-function(model, hypothesis.matrix, rhs=0, 
         summary.model=summary(model, corr = FALSE),
+	test=c("F", "Chisq"), vcov=NULL,
         white.adjust=FALSE, error.SS, error.df, ...) {
-    # last modified by J.Fox 13 Dec 2000
     if (is.aliased(model)) stop ("One or more terms aliased in model.")
-    s2<-if (missing(error.SS)) summary.model$sigma^2
-        else error.SS/error.df
-    V<-if (white.adjust==FALSE) summary.model$cov.unscaled
-        else hccm(model, type=white.adjust)/s2
-    b<-coefficients(model)
-    L<-if (is.null(dim(hypothesis.matrix))) t(hypothesis.matrix)
-        else hypothesis.matrix
-    q<-nrow(L)
-    SSH<-t(L %*% b - rhs) %*% inv(L %*% V %*% t(L)) %*% (L %*% b - rhs)
-    f<-SSH/(q*s2)
-    df<-if (missing(error.df)) model$df.residual
-        else error.df
-    p<-1-pf(f, q, df)
-    result<-list(SSH=SSH[1,1], SSE=s2*df, f=f[1,1], Df=c(q, df), p=p[1,1])
-    class(result)<-"F.test"
-    result
+
+    ## obtain RSS and df
+    df <- if (missing(error.df)) model$df.residual else error.df
+    error.SS <- if(missing(error.SS)) summary.model$sigma^2 * df
+    s2 <- error.SS/df
+
+    ## obtain covariance matrix:
+    ## can be NULL (default: then white.adjust is used as before),
+    ## function or matrix
+    if(identical(white.adjust, TRUE)) white.adjust <- "hc3"
+    V<-if(is.null(vcov)) {
+         if (!is.character(white.adjust)) s2 * summary.model$cov.unscaled
+         else hccm(model, type=white.adjust)
+       } else {
+         if(is.function(vcov)) vcov(model)
+	 else vcov
+       }
+
+    b <- coefficients(model)
+    L <- if (is.null(dim(hypothesis.matrix))) t(hypothesis.matrix)
+         else hypothesis.matrix
+    q <- nrow(L)
+
+    SSH <- as.vector(t(L %*% b - rhs) %*% inv(L %*% V %*% t(L)) %*% (L %*% b - rhs))
+
+    ## support both asymptotic Chisq test and (approximate) finite sample F test
+    ## if df not finite, choose to use Chisq test
+    test <- match.arg(test)
+    if(!(is.finite(df) && df > 0)) test <- "Chisq"
+
+    ## put together output
+    title <- "Linear hypothesis test\n"
+    topnote <- paste("Model 1: ", deparse(formula(model)), "\n",
+                     "Model 2: restricted model", sep = "")
+    rval <- matrix(rep(NA, 8), ncol = 4)
+    colnames(rval) <- c("Res.Df", "Df", test, paste("Pr(>", test, ")", sep = ""))
+    rownames(rval) <- 1:2
+    rval[,1] <- c(df, df+q)
+    
+    if(test == "F") {
+      f <- SSH/q
+      p <- pf(f, q, df, lower.tail = FALSE)
+      rval[2,2:4] <- c(-q, f, p)
+    } else {
+      p <- pchisq(SSH, q, lower.tail = FALSE)
+      rval[2,2:4] <- c(-q, SSH, p)
+    }
+    
+    ## if default vcov estimate was used, add RSS to output
+    if(is.null(vcov)) {
+      rval2 <- matrix(rep(NA, 4), ncol = 2)
+      colnames(rval2) <- c("RSS", "Sum of Sq")
+      rval2[,1] <- c(error.SS, error.SS + SSH * s2)
+      rval2[2,2] <- -diff(rval2[,1])
+      rval <- cbind(rval, rval2)[,c(1, 5, 2, 6, 3, 4)]
+    }    
+    
+    rval <- structure(as.data.frame(rval), heading = c(title, topnote),
+	    class = c("anova", "data.frame"))
+    return(rval)
     }
 
 linear.hypothesis.glm<-function(model, hypothesis.matrix, rhs=0, 
-        summary.model=summary(model, corr = FALSE), ...) {
-    # last modified by J.Fox 13 Dec 2000
+        summary.model=summary(model, corr = FALSE),
+	test=c("Chisq", "F"), vcov=NULL, error.df, ...) {
     if (is.aliased(model)) stop ("One or more terms aliased in model.")
-    V<-summary.model$dispersion*summary.model$cov.unscaled
-    b<-coefficients(model)
-    L<-if (is.null(dim(hypothesis.matrix))) t(hypothesis.matrix)
-        else hypothesis.matrix
-    q<-nrow(L)
-    Wald<-t(L %*% b - rhs) %*% inv(L %*% V %*% t(L)) %*% (L %*% b - rhs)
-    p<-1-pchisq(Wald, q)
-    result<-list(test="Wald Test",ChiSquare=Wald[1,1], Df=q, p=p[1,1])
-    class(result)<-"chisq.test"
-    result
-    }
- 
 
- print.chisq.test<-function(x, ...){
-    title<-if (!is.null(x$test)) x$test else "Chisquare Test"
-    cat(title,"\n")
-    if (!is.null(x$formula)) cat(x$formula.name, 
-        "formula:", as.character(x$formula), "\n")
-    cat("Chisquare =", x$ChiSquare,"   Df =", x$Df,
-        "    p =", x$p, "\n")
-    invisible(x)
+    ## obtain df
+    df <- if (missing(error.df)) model$df.residual else error.df
+
+    ## compute covariance matrix (see linear.hypothesis.lm)
+    V<-if(is.null(vcov)) {
+         summary.model$dispersion*summary.model$cov.unscaled
+       } else {
+         if(is.function(vcov)) vcov(model)
+	 else vcov
+       }
+    
+    b <- coefficients(model)
+    L <- if (is.null(dim(hypothesis.matrix))) t(hypothesis.matrix)
+         else hypothesis.matrix
+    q <- nrow(L)
+    SSH <- as.vector(t(L %*% b - rhs) %*% inv(L %*% V %*% t(L)) %*% (L %*% b - rhs))
+
+    ## support both asymptotic Chisq test and (approximate) finite sample F test
+    ## if not finite, choose to use Chisq test
+    test <- match.arg(test)
+    if(!(is.finite(df) && df > 0)) test <- "Chisq"
+
+    ## set up return value
+    title <- "Linear hypothesis test\n"
+    topnote <- paste("Model 1: ", deparse(formula(model)), "\n",
+                     "Model 2: restricted model", sep = "")
+    rval <- matrix(rep(NA, 8), ncol = 4)
+    colnames(rval) <- c("Res.Df", "Df", test, paste("Pr(>", test, ")", sep = ""))
+    rownames(rval) <- 1:2
+    rval[,1] <- c(df, df+q)
+    
+    if(test == "F") {
+      f <- SSH/q
+      p <- pf(f, q, df, lower.tail = FALSE)
+      rval[2,2:4] <- c(-q, f, p)
+    } else {
+      p <- pchisq(SSH, q, lower.tail = FALSE)
+      rval[2,2:4] <- c(-q, SSH, p)
     }
- 
- print.F.test<-function(x, ...){
-    title<-if (!is.null(x$test)) x$test else "F-Test"
-    cat(title,"\n")
-    if (!is.null(x$formula)) cat(x$formula.name, 
-        "formula:", as.character(x$formula), "\n")
-    cat("SS =", x$SSH, "    SSE =", x$SSE, "    F =", x$f,
-        " Df =", x$Df[1], "and", x$Df[2], "    p =", x$p, "\n")
-    invisible(x)
+
+    rval <- structure(as.data.frame(rval), heading = c(title, topnote),
+	    class = c("anova", "data.frame"))
+    return(rval)
     }
--- R/vif.R
+++ R/vif.R
@@ -7,7 +7,7 @@ vif<-function(mod){
 
 
 vif.lm <- function(mod) {
-  # version by H. Nilsson, last modified 17 Jan 05 by J. Fox
+  # version by H. Nilsson, last modified 23 Jan 05 by J. Fox
   v <- vcov(mod)
   assign <- attributes(model.matrix(mod))$assign
   if (names(coefficients(mod)[1]) == "(Intercept)") {
@@ -19,12 +19,14 @@ vif.lm <- function(mod) {
   n.terms <- length(terms)
   if (n.terms < 2) stop("model contains fewer than 2 terms")
   R <- cov2cor(v)
+  detR <- det(R)
   result <- matrix(0, n.terms, 3)
   rownames(result) <- terms
   colnames(result) <- c("GVIF", "Df", "GVIF^(1/2Df)")
   for (term in 1:n.terms) {
     subs <- which(assign == term)
-    result[term, 1] <- det(as.matrix(R[subs, subs])) * det(as.matrix(R[-subs, -subs])) / det(R)
+    result[term, 1] <- det(as.matrix(R[subs, subs])) *
+        det(as.matrix(R[-subs, -subs])) / detR
     result[term, 2] <- length(subs)
   }
   if (all(result[, 2] == 1)) result <- result[, 1]
