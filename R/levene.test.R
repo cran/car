df2c4de663d66a1d911847d5377cfef355c177ab
John Fox <jfox@mcmaster.ca>
2007-08-19 00:00:00 +0000

    version 1.2-2

--- R/levene.test.R
+++ R/levene.test.R
@@ -1,8 +1,15 @@
 # moved from Rcmdr 13 July 2004
+# last modified 26 March 2007 by J. Fox
 
 # the following function slightly modified from Brian Ripley via R-help
 
 levene.test <- function (y, group) {
+    if (!is.numeric(y)) 
+        stop(deparse(substitute(y)), " is not a numeric variable")
+    if (!is.factor(group)) {
+        warning(deparse(substitute(group)), " coerced to factor.")
+        group <- as.factor(group)
+        }
     meds <- tapply(y, group, median, na.rm = TRUE)
     resp <- abs(y - meds[group])
     table <- anova(lm(resp ~ group))[, c(1, 4, 5)]
