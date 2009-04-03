3ea00fc3c4b0cb3b70566794b4c56a2312a82c35
John Fox <jfox@mcmaster.ca>
2009-04-03 09:41:58 +0000

    version 1.2-13

--- R/recode.R
+++ R/recode.R
@@ -1,46 +1,48 @@
 # recode function (J. Fox)
-# last modified 22 Oct 2006
+# last modified 2 April 2009
 
 recode<-function(var, recodes, as.factor.result, levels){
-    recode.list<-rev(strsplit(recodes, ";")[[1]])
-    is.fac<-is.factor(var)
-    if (missing(as.factor.result)) as.factor.result <- is.fac
-    if (is.fac) var<-as.character(var)
-    result<-var
-    if (is.numeric(var)) {
-        lo<-min(var, na.rm=TRUE)
-        hi<-max(var, na.rm=TRUE)
-        }
-    for (term in recode.list){
-        if (0<length(grep(":", term))) {
-            range<-strsplit(strsplit(term, "=")[[1]][1],":")
-            low<-eval(parse(text=range[[1]][1]))
-            high<-eval(parse(text=range[[1]][2]))
-            target<-eval(parse(text=strsplit(term, "=")[[1]][2]))
-            result[(var>=low)&(var<=high)]<-target
-            }
-        else if (0<length(grep("else", term))) {
-            target<-eval(parse(text=strsplit(term, "=")[[1]][2]))
-            result[1:length(var)]<-target
-            }
-        else {
-            set<-eval(parse(text=strsplit(term, "=")[[1]][1]))
-            target<-eval(parse(text=strsplit(term, "=")[[1]][2]))
-            for (val in set){
-                if (is.na(val)) result[is.na(var)]<-target
-                    else result[var==val]<-target
-                }
-            }
-        }
-    if (as.factor.result) {
-        result <- if (!missing(levels)) factor(result, levels=levels) 
-            else as.factor(result)
-            }
-        else if (!is.numeric(result)) {
-            result.valid <- na.omit(result)
-            if (length(result.valid) == length(grep("[0-9]",result.valid)))
-                result <- as.numeric(result)
-            }
-    result
-    }
- 
+	recode.list <- rev(strsplit(recodes, ";")[[1]])
+	is.fac <- is.factor(var)
+	if (missing(as.factor.result)) as.factor.result <- is.fac
+	if (is.fac) var <- as.character(var)
+	result <- var
+	if (is.numeric(var)) {
+		lo <- min(var, na.rm=TRUE)
+		hi <- max(var, na.rm=TRUE)
+	}
+	for (term in recode.list){
+		if (0 < length(grep(":", term))) {
+			range <- strsplit(strsplit(term, "=")[[1]][1],":")
+			low <- eval(parse(text=range[[1]][1]))
+			high <- eval(parse(text=range[[1]][2]))
+			target <- eval(parse(text=strsplit(term, "=")[[1]][2]))
+			result[(var >= low) & (var <= high)] <- target
+		}
+		else if (0 < length(grep("else", term))) {
+			target <- eval(parse(text=strsplit(term, "=")[[1]][2]))
+			result[1:length(var)] <- target
+		}
+		else {
+			set <- eval(parse(text=strsplit(term, "=")[[1]][1]))
+			target <- eval(parse(text=strsplit(term, "=")[[1]][2]))
+			for (val in set){
+				if (is.na(val)) result[is.na(var)] <- target
+				else result[var == val] <- target
+			}
+		}
+	}
+	if (as.factor.result) {
+		result <- if (!missing(levels)) factor(result, levels=levels) 
+			else as.factor(result)
+	}
+	else if (!is.numeric(result)) {
+		result.valid <- na.omit(result)
+		opt <- options("warn"=-1)
+		result.valid <- as.numeric(result.valid)
+		options(opt)
+		if (!any(is.na(result.valid))) result <- as.numeric(result)
+	}
+	result
+}
+
